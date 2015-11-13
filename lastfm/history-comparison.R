a21a75f298fee5eea35b3baaf1db4e30346e809b
Vlad Golubev <golubev.vld@gmail.com>
2015-11-14 00:02:34 +0200

    Add another comparison plot

--- lastfm/history-comparison.R
+++ lastfm/history-comparison.R
@@ -1,29 +1,30 @@
 library(dplyr)
+library(ggvis)
 library(data.table)
 
 LAST.FM.samosfator <- tbl_df(fread("lastfm-samosfator-dump-2015-11-13.csv", sep = ",", header = TRUE))
 LAST.FM.oasispo <- tbl_df(fread("lastfm-oasispo-dump-2015-11-13.csv", sep = ",", header = TRUE))
 
+days_of_week <- c("понеділок", "вівторок", "середа", "четвер", "п'ятниця", "субота", "неділя")
 
 samosfator.hours_distribution <- LAST.FM.samosfator %>%
-  group_by(hour = format(as.POSIXct(LAST.FM.samosfator$date), "%H")) %>%
+  group_by(day = weekdays(as.POSIXct(LAST.FM.samosfator$date))) %>%
   summarise(plays = n()) %>%
-  arrange(desc(plays))
+  arrange(factor(samosfator.hours_distribution$day, levels = days_of_week)) %>%
+  mutate(ratio = plays / nrow(LAST.FM.samosfator)) %>%
+  mutate(day = factor(day, levels = days_of_week))
 
 oasispo.hours_distribution <- LAST.FM.oasispo %>%
-  group_by(hour = format(as.POSIXct(LAST.FM.oasispo$date), "%H")) %>%
+  group_by(day = weekdays(as.POSIXct(LAST.FM.oasispo$date))) %>%
   summarise(plays = n()) %>%
-  arrange(desc(plays))
+  arrange(factor(oasispo.hours_distribution$day, levels = days_of_week)) %>%
+  mutate(ratio = plays / nrow(LAST.FM.oasispo)) %>%
+  mutate(day = factor(day, levels = days_of_week))
 
-plot(smooth.spline(samosfator.hours_distribution),
-     type = "l",
-     xlab = "Години доби (1-24)",
-     ylab = "Скроблів",
-     main = "Кількість скроблів по годинах",
-     col = "blue")
+samosfator.hours_distribution %>%
+  ggvis(~day, ~ratio, fill := "blue") %>%
+  layer_points() %>%
+  layer_points(data = oasispo.hours_distribution,
+               ~day, ~ratio, fill := "red")
 
-lines(smooth.spline(oasispo.hours_distribution), col = "red")
-
-legend('topleft', c("samosfator", "oasispo"), 
-       lty=1, col=c("blue", "red"), bty='n', cex=.75)
 
