aaf1aed888af7dc12cff003400e31b530859b441
Vlad Golubev <golubev.vld@gmail.com>
2015-11-26 18:39:28 +0200

    Update TIMS tasks with corrections

--- tims/8.R
+++ tims/8.R
@@ -3,17 +3,18 @@ n <- c(3, 6, 15, 18, 5, 3)
 # Вмістимо два графіка на екрані
 par(mfrow = c(2, 1))
 
-plot(type = "h",
-     lwd = 10,
-     x = seq(from = 7.2, to = 22.2, by = 3),
-     y = n / 3,
-     log = "y",
-     xlim = c(0, 25),
-     main = "Гістограма частот")
+barplot(n / 3,
+        space = c(4.2 / 3, 0, 0, 0, 0, 0),
+        width = c(3, 3, 3, 3, 3, 3),
+        xlim = c(0, 25),
+        main = "Гістограма частот")
+axis(side = 1)
 grid()
-plot(n / sum(n) / 3,
-     type = "h",
-     lwd = 10,
-     log = "y",
-     main = "Гістограма відносних частот")
+
+barplot(n / sum(n) / 3,
+        space = c(4.2 / 3, 0, 0, 0, 0, 0),
+        width = c(3, 3, 3, 3, 3, 3),
+        xlim = c(0, 25),
+        main = "Гістограма відносних частот")
+axis(side = 1)
 grid()
