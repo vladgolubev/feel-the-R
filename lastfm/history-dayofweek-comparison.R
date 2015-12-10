a25be7a2f34d6ad390023baaf858fac710454c98
Vlad Golubev <golubev.vld@gmail.com>
2015-12-10 15:22:28 +0200

    Lastfm hours comparison plot refactoring

--- lastfm/history-dayofweek-comparison.R
+++ lastfm/history-dayofweek-comparison.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(ggvis)
 library(data.table)
 
-LAST.FM.samosfator <- tbl_df(fread("lastfm-samosfator-dump-2015-11-13.csv", sep = ",", header = TRUE))
-LAST.FM.oasispo <- tbl_df(fread("lastfm-oasispo-dump-2015-11-13.csv", sep = ",", header = TRUE))
+LAST.FM.samosfator <- tbl_df(fread("lastfm/dumps/lastfm-samosfator-dump-2015-11-13.csv", sep = ",", header = TRUE))
+LAST.FM.oasispo <- tbl_df(fread("lastfm/dumps/lastfm-oasispo-dump-2015-11-13.csv", sep = ",", header = TRUE))
 
 days_of_week <- c("понеділок", "вівторок", "середа", "четвер", "п'ятниця", "субота", "неділя")
 
@@ -21,8 +21,10 @@ oasispo.dayofweek_distribution <- LAST.FM.oasispo %>%
          day = factor(day, levels = days_of_week)) %>%
   select(-plays)
 
-samosfator.dayofweek_distribution %>%
-  ggvis(~day, ~ratio, fill := "blue") %>%
-  layer_points() %>%
-  layer_points(data = oasispo.dayofweek_distribution,
-               ~day, ~ratio, fill := "red")
+comparison <- rbind(samosfator.dayofweek_distribution %>% mutate(username = "samosfator"),
+                    oasispo.dayofweek_distribution %>% mutate(username = "oasispo"))
+
+comparison %>%
+  group_by(username) %>%
+  ggvis(~day, ~ratio, stroke = ~username, strokeWidth := 4) %>%
+  layer_lines()
